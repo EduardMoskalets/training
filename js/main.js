a38da6d75fa54caa72b82ed82fa933adbd83e38d
Eduard <edik.moskalets@gmail.com>
2025-09-09 18:05:18 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const x = Math.round(Math.random() * 100);
+// const x = Math.round(Math.random() * 100);
 
-console.log(x);
+// console.log(x);
 // =================================================================================================================
 
+let array = []
+
+
+function includesElement(array, element) {
+    return array.indexOf(element) !== -1;
+}
